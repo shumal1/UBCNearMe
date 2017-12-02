71e8ae709e9ee375bab6869f7188574b8d823fb1
SuperApple <judy.622@me.com>
2017-12-02 11:39:59 -0800

    abstract room basic test

--- src/Model/AbstractResource.java
+++ src/Model/AbstractResource.java
@@ -1,4 +1,6 @@
 package Model;
 
 public abstract class AbstractResource implements IResource {
+    public int figure;
+    public int figure1;
 }
--- src/Model/AbstractRoom.java
+++ src/Model/AbstractRoom.java
@@ -1,4 +1,27 @@
 package Model;
 
 public class AbstractRoom extends AbstractResource {
+    protected Building building;
+    protected int floor;
+    protected String roomNumber;
+
+
+    public AbstractRoom (){
+        building = null;
+        floor = 0;
+        roomNumber = "";
+    }
+
+    public void setBuilding(Building building){
+        this.building = building;
+    }
+
+    public void setFloor (int floor){
+        this.floor = floor;
+    }
+
+    public void setRoomNumber (String roomNumber){
+        this.roomNumber = roomNumber;
+    }
+
 }

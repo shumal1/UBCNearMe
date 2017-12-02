5239bc5ee471ff2fcf10d431d6424e8ccb0575d6
shizuko <aka-shizuko@hotmail.com>
2017-12-02 13:16:32 -0800

    ui test

---
+++ src/ui/testMap.java
@@ -0,0 +1,54 @@
+package ui;
+
+import com.teamdev.jxmaps.*;
+import com.teamdev.jxmaps.swing.MapView;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class testMap extends MapView {
+    public testMap(MapViewOptions options) {
+        super(options);
+        setOnMapReadyHandler(new MapReadyHandler() {
+            @Override
+            public void onMapReady(MapStatus status) {
+                if (status == MapStatus.MAP_STATUS_OK) {
+                    final Map map = getMap();
+                    map.setZoom(5.0);
+                    GeocoderRequest request = new GeocoderRequest(map);
+                    request.setAddress("Kharkiv, UA");
+
+                    getServices().getGeocoder().geocode(request, new GeocoderCallback(map) {
+                        @Override
+                        public void onComplete(GeocoderResult[] result, GeocoderStatus status) {
+                            if (status == GeocoderStatus.OK) {
+                                map.setCenter(result[0].getGeometry().getLocation());
+                                Marker marker = new Marker(map);
+                                marker.setPosition(result[0].getGeometry().getLocation());
+
+                                final InfoWindow window = new InfoWindow(map);
+                                window.setContent("Hello, World!");
+                                window.open(map, marker);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+
+        MapViewOptions options = new MapViewOptions();
+        options.importPlaces();
+        final testMap mapView = new testMap(options);
+
+        JFrame frame = new JFrame("JxMaps - Hello, World!");
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(mapView, BorderLayout.CENTER);
+        frame.setSize(700, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}

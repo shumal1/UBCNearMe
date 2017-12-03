83dac81b88c39a82fcee360c491ed62e81d311f3
shizuko <aka-shizuko@hotmail.com>
2017-12-02 16:42:22 -0800

    SelectionPanel in view

--- src/ui/nearME.java
+++ src/ui/nearME.java
@@ -9,16 +9,34 @@ import com.teamdev.jxmaps.MapStatus;
 import com.teamdev.jxmaps.MapTypeControlOptions;
 import com.teamdev.jxmaps.javafx.MapView;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
 public class nearME extends Application{
+    // Creation of a JavaFX map view
+    final MapView mapView = new MapView();
+    private StackPane stackPane;
+    private BorderPane border;
+    private VBox selectionPanel;
+
     @Override
     public void start(final Stage primaryStage) {
-        // Creation of a JavaFX map view
-        final MapView mapView = new MapView();
+        border = new BorderPane();
+        selectionPanel = new VBox();
+
+        primaryStage.setTitle("UBCNearMe");
 
         // Setting of a ready handler to MapView object. onMapReady will be called when map initialization is done and
         // the map object is ready to use. Current implementation of onMapReady customizes the map object.
@@ -47,7 +65,30 @@ public class nearME extends Application{
             }
         });
 
-        Scene scene = new Scene(new BorderPane(mapView), 700, 500);
+        selectionPanel = new VBox();
+        selectionPanel.setPadding(new Insets(10));
+        selectionPanel.setSpacing(8);
+
+        Text title = new Text("Resources");
+        title.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+        selectionPanel.getChildren().add(title);
+
+        CheckBox options[] = new CheckBox[]{
+                new CheckBox("Washroom"),
+                new CheckBox("Water Fountain"),
+                new CheckBox("Study Room"),
+                new CheckBox("Vending Machine")
+        };
+
+        for (int i=0; i<4; i++) {
+            VBox.setMargin(options[i], new Insets(0, 0, 0, 8));
+            selectionPanel.getChildren().add(options[i]);
+        }
+
+        border.setCenter(mapView);
+        border.setRight(selectionPanel);
+
+        Scene scene = new Scene(border, 700, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
